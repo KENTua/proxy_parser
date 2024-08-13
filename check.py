c44927cf9a2d803b47d3cf4a50d96f0edbcb9499
KENTua <kent3250@gmail.com>
2024-08-13 16:22:34 +0300

    checker

--- check.py
+++ check.py
@@ -1,13 +1,47 @@
 from requests import get
+from rich.console import Console
+from os import cpu_count
+from multiprocessing.dummy import Pool
 
+pool = Pool(60)#cpu_count()*2)  # threads count
+console=Console()
+TIMEOUT =2
 with open('proxies.txt','r') as f:
     proxies = f.read().rstrip('\n').split('\n')
 
 
-for proxy in proxies:
-    print(proxy)
+def check(proxy:str)->str:
+    result='error'
     try:
-        resp=get('https://ident.me',proxies={'https':'https://'+proxy}).text
-        print(resp)
+        get('https://ident.me',proxies={'https':'https://'+proxy},timeout=TIMEOUT)
+        #text=await resp.text
+        result='https://'+proxy
     except:
-        pass
+        try:
+            get('https://ident.me',proxies={'https':'socks4://'+proxy},timeout=TIMEOUT)
+            #text=await resp.text
+            result='socks4://'+proxy
+        except:
+            pass
+    return result
+
+
+def main():
+    goods=[]
+    print('to check:',len(proxies))
+    results = pool.map(check, proxies)
+    pool.close()
+    pool.join()
+    for result in results:
+        if result!='error':
+            style='bold green'
+            goods.append(result)
+        else:
+            style='bold red'
+        #console.print(result,style=style)
+    with open('checked.txt', 'w') as f:
+        f.write('\n'.join(goods))
+        
+
+if __name__=='__main__':
+    main()
--- modules/test.py
+++ modules/test.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
-from requests import get,post
+import aiohttp
+import asyncio
+
+from aiohttp_socks import ProxyConnector
 from bs4 import BeautifulSoup as bs
 from re import findall
 from rich import print
+TIMEOUT =2
+#asyncio_loop=asyncio.get_running_loop()
+
 
 #,proxies={'http':'socks5://127.0.0.1:9050'} 34
 
-html=get('https://www.proxy-list.download/api/v1/get?type=socks4',timeout=5).text
-print(html)
+async def check(proxy:str,session)->str:
+    result='error'
+    resp=await session.get('https://ident.me',timeout=TIMEOUT)
+    #text=await resp.text
+    result='https://'+proxy
+    return result
 
-exit()
-soup = bs(html, 'lxml')
-htmlText = soup.find('body').get_text()
 
-parsed = []
-parsedRaw = findall(r'((\d{1,3}\.){3}\d{1,3}:\d{2,5})', htmlText)
-print(html)
-for proxy in parsedRaw:
-  parsed.append(proxy[0])
-  print(proxy[0])
+async def create_task(proxy:str):
+  conn=ProxyConnector.from_url('socks4://127.0.0.1:9050')
+  tasks=[]
+  print(conn)
+  async with aiohttp.ClientSession(connector=conn) as session:
+    tasks.append(asyncio.create_task(check(proxy,session)))
+    return await asyncio.gather(*tasks)
 
-#print(parsed)
+async def main():
+    proxies=['127.0.0.1:9050']
+    for proxy in proxies:
+        print(await create_task(proxy))
+
+
+asyncio.run(main())
